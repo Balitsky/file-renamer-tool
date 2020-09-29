2d9817cd5202faeaac24535dfdb82c8f1792b6d4
Oleksandr Balitskyi <Oleksandr.Balitskyi@playtech.com>
2020-09-29 23:12:27 +0300

    jquery update

--- src/client.js
+++ src/client.js
@@ -1,14 +1,16 @@
-class Theme {
-  static get FADE_IN() { return "fade-in" };
-  static get FADE_OUT() { return "fade-out" };
-  static get DISABLED() { return "disable" };
-  static get ENABLED() { return "enable" };
-}
-const TOOLS = document.getElementsByClassName("tool_container");
+const TOOLS = $('.tool_container');
 
-document.getElementById("close-btn").addEventListener("click", function (e) {
-  window.close();
-});
+function init(){
+  $('#default_active').attr('id', 'disable_button');
+  TOOLS.each(function (index, tool) {
+    $(tool).fadeOut(0);
+  })
+  $('.second_tool').fadeIn(0)
+
+  $('#close-btn').on('click', function(){
+    window.close();
+  });
+}
 
 function applyStyle(element, theme) {
   if (element) {
@@ -16,39 +18,25 @@ function applyStyle(element, theme) {
   }
 }
 
-function openTool(clickedButton, tool) {
-  applyStyle(document.getElementById("disable_button"), Theme.ENABLED)
-  applyStyle(clickedButton, "disable_button");
+function openTool(button, currentTool) {
+  $('#disable_button').removeAttr('id');
+  $(button).attr('id', 'disable_button');
 
-  Array.prototype.forEach.call(TOOLS, function (tool) {
-    applyStyle(tool, Theme.FADE_OUT)
-  });
+  currentTool = $('.' + currentTool)
+  currentTool.fadeIn();
 
-  var activeTool = document.getElementsByClassName(tool)[0]
-  applyStyle(activeTool, Theme.FADE_IN);
+  TOOLS.each(function (index, tool) {
+    tool = $(tool);
+    if(!tool.is(currentTool)){
+      tool.fadeOut();
+    }
+  })
 }
 
-Array.prototype.forEach.call(TOOLS, function (tool) {
-  tool.ontransitionend = () => {
-    switch (tool.getAttribute('id')) {
-      case Theme.FADE_IN: applyStyle(tool, Theme.ENABLED); break;
-      case Theme.FADE_OUT: applyStyle(tool, Theme.DISABLED); break;
-    }
-  };
-});
 
-document.getElementById("default_active").click();
-/* $('.toggle').click(function(e){
-  e.preventDefault(); // The flicker is a codepen thing
+$('.toggle').click(function(e){
   $(this).toggleClass('toggle-on');
   $('.sign_word').toggleClass('active12');
-  console.log($('.sign_word'))
 });
-$('.btn1').click(function(e){
-  $('.first_tool').animate({ opacity: "show" }, "slow");
-  $('.second_tool').animate({ opacity: "hide" }, "slow");
-});
-$('.btn2').click(function(e){
-  $('.first_tool').animate({ opacity: "hide" }, "slow");
-  $('.second_tool').animate({ opacity: "show" }, "slow");
-}); */
+
+init();
