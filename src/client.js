8ff694c68aea911acc78347a762acfca82d15679
Oleksandr Balitskyi <Oleksandr.Balitskyi@playtech.com>
2020-09-30 13:56:18 +0300

    default tool logic reworked, added toggle buttons

--- src/client.js
+++ src/client.js
@@ -1,11 +1,15 @@
 const TOOLS = $('.tool_container');
 
 function init(){
+  var toolNameRegex = new RegExp("([a-zA-Z_]+)(?=['])");
+  var defTool = toolNameRegex.exec($('#default_active').attr('onClick')).pop()
   $('#default_active').attr('id', 'disable_button');
   TOOLS.each(function (index, tool) {
     $(tool).fadeOut(0);
   })
-  $('.second_tool').fadeIn(0)
+  $('.' + defTool).fadeIn(0)
+  $('.toggle').trigger('click');
+
 
   $('#close-btn').on('click', function(){
     window.close();
@@ -34,9 +38,20 @@ function openTool(button, currentTool) {
 }
 
 
-$('.toggle').click(function(e){
+$('.toggle').on('click', function(e){
   $(this).toggleClass('toggle-on');
-  $('.sign_word').toggleClass('active12');
+});
+$('.toggle#regular').on('click', function(e){
+  $('#regular').toggleClass('active12');
+});
+$('.toggle#hovered').on('click', function(e){
+  $('#hovered').toggleClass('active12');
+});
+$('.toggle#pressed').on('click', function(e){
+  $('#pressed').toggleClass('active12');
+});
+$('.toggle#disabled').on('click', function(e){
+  $('#disabled').toggleClass('active12');
 });
 
 init();
